5b27651cf8d4dc175629b93af624ce61841501d3
berkan-ridvan <berkan5551@gmail.com>
2023-11-15 19:39:07 +0300

    login and signup designs finished

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './SignIn.scss'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className='container-signin'>
+      <div className="login-div">
+        <div className="login-div-input">
+          <h3>Giriş Yap</h3>
+          <h5>Kullanıcı Adı :</h5>
+          <input placeholder='Account Id' type="text" />
+          <h5>Şifre :</h5>
+          <input placeholder='Password' type="password" name="password" id="password" />
+          <h4>Şifreni mi unuttun ? <a href="#"><u>Şifremi unuttum!</u></a></h4>
+        </div>
+        <div className="login-div-button">
+          <button className='active'>Giriş Yap</button>
+          <button>Kayıt Ol</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import './SignUp.scss'
+
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='container-signup'>
+      <div className="login-div">
+        <div className="login-div-input">
+          <h3>Kayıt Ol</h3>
+          <h5>Kullanıcı Adı :</h5>
+          <input placeholder='Account Id' type="text" />
+          <h5>E-Posta :</h5>
+          <input placeholder='E-mail' type="text" />
+          <h5>Şifre :</h5>
+          <input placeholder='Password' type="password" name="password" id="password" />
+          <h5>Şifre :</h5>
+          <input placeholder='Password' type="password" name="password" id="password" />
+          <h4>* Şifreler birbiriyle aynı olmalı !</h4>
+        </div>
+        <div className="login-div-button">
+          <button>Kayıt Ol</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
