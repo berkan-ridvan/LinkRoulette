03836c68d64b78460ac205ade56f43b8e37f5a7c
canzeyn <zeyn1616@outlook.com>
2023-11-15 15:38:07 +0300

    paketler kuruldu componentler ayrıldı
    tasarım yapılacak

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainComponent from "./components/MainComponent/MainComponent";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+import Profile from "./components/Profile/Profile";
+import AddFilm from "./components/AddFilm/AddFilm";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainComponent />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/AddFilm" element={<AddFilm />} />
+
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/components/AddFilm/AddFilm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddFilm = () => {
+  return (
+    <div>AddFilm</div>
+  )
+}
+
+export default AddFilm
